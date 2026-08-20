b7ace0c3af35cd680028b1716f04ea1e88ac7828
kurtbot-release <kurtbot@users.noreply.github.com>
2026-08-20 14:39:26 +0000

    chore(release): update Homebrew formula for v0.5.1 [skip ci]

--- Formula/glassline.rb
+++ Formula/glassline.rb
@@ -7,7 +7,7 @@ class Glassline < Formula
   on_macos do
     on_arm do
       url "https://github.com/kurtbot/glassline/releases/download/v0.5.1/glassline-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_MAC_ARM64"
+      sha256 "aaacc4d8b0f7ff46b94db04662f5ec24b4ae8c58ee3e79e847b79bd5a09d63a3"
     end
     on_intel do
       url "https://github.com/kurtbot/glassline/releases/download/v0.5.1/glassline-x86_64-apple-darwin.tar.gz"
@@ -18,7 +18,7 @@ class Glassline < Formula
   on_linux do
     on_arm do
       url "https://github.com/kurtbot/glassline/releases/download/v0.5.1/glassline-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "5d1060e7bee491f6038931c4030d865ac33d0b651a3f2a5331250fc8579092e1"
     end
     on_intel do
       url "https://github.com/kurtbot/glassline/releases/download/v0.5.1/glassline-x86_64-unknown-linux-gnu.tar.gz"
